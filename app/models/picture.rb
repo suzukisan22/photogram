95e06501fbb0f6e129032b17e334e4361b0e547c
Ryota <ryt.0081@gmail.com>
2016-10-05 23:41:22 +0900

    シードデータ編集

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,7 @@ class Picture < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
   validates :avatar, presence: true
+  validates :comment, length: { maximum: 255 }
 
   belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |n|
+  name = Faker::Book.author
+  email = Faker::Internet.email
+  password = "password"
+  avatar = Faker::Avatar.image
+  comment = Faker::StarWars.quote
+  uid = Faker::Crypto.md5
+  User.create!(name: name,
+               uid: uid,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               avatar: avatar
+               )
+  Picture.create!(avatar: "enu-thumbnail2.jpg",
+                  comment: comment
+  )
+end
