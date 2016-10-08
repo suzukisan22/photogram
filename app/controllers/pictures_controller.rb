42c9834cd64d7b885570939ec6a6012ee906e90d
Ryota <ryt.0081@gmail.com>
2016-10-08 18:41:08 +0900

    メッセージを日本語化に修正

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -31,7 +31,7 @@ class PicturesController < ApplicationController
     @picture.user_id = current_user.id
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.html { redirect_to @picture, notice: '投稿の保存に成功しました' }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.html { redirect_to @picture, notice: '投稿の更新に成功しました' }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
@@ -59,7 +59,7 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to pictures_url, notice: '投稿の削除に成功しました' }
       format.json { head :no_content }
     end
   end
