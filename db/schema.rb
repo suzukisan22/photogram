1fb41b088e7e46a84001935f9e6413633d291146
Ryota <ryt.0081@gmail.com>
2016-11-17 21:11:07 +0900

    ユーザー情報に公開・非公開機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115122304) do
+ActiveRecord::Schema.define(version: 20161117115532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20161115122304) do
     t.string   "image_url"
     t.string   "avatar"
     t.string   "username"
+    t.integer  "secret",                 default: 0
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
