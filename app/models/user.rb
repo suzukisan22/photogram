640efce59ffaad4eb18f19d3bf01d4dce6f20e08
Ryota <ryt.0081@gmail.com>
2016-11-19 00:41:40 +0900

    フォロー数、フォローワー数を表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
+
   has_many :pictures
   # コメントモデルのアソシエーションを設定
   has_many :comments, dependent: :destroy
@@ -15,8 +16,8 @@ class User < ActiveRecord::Base
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: 'Relationship', dependent: :destroy
 
-  has_many :followed_user, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_relationship, source: :follower
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
     user = User.where(provider: auth.provider, uid: auth.uid).first
