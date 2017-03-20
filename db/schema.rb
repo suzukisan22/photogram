84b4f9cbd308c5c30a7d4bc12c321d6177c4114b
Ryota <ryt.0081@gmail.com>
2017-03-20 19:40:41 +0900

    add column introduction at users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127111211) do
+ActiveRecord::Schema.define(version: 20170320103913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20161127111211) do
     t.string   "avatar"
     t.string   "username"
     t.integer  "secret",                 default: 0
+    t.text     "introduction"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
