259e60427bc6f1636f8d46e03063fa440f257468
Ryota <ryt.0081@gmail.com>
2017-06-18 19:38:55 +0900

    add aliases at model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -11,6 +11,8 @@
 #
 
 class Picture < ActiveRecord::Base
+  acts_as_taggable_on :labels # post.label_list が追加される
+  acts_as_taggable            # acts_as_taggable_on :tags のエイリアス
   mount_uploader :avatar, AvatarUploader
 
   validates :avatar, presence: true
